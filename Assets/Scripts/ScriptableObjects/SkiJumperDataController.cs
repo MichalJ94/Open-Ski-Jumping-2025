72a015cbbc4220e9ded3e3f0dbc9b6a6a2fe623a
MichalJ94 <167618276+MichalJadczak@users.noreply.github.com>
2025-07-16 16:37:39 +0200

    Skis WIP

--- Assets/Scripts/ScriptableObjects/SkiJumperDataController.cs
+++ Assets/Scripts/ScriptableObjects/SkiJumperDataController.cs
@@ -151,6 +151,7 @@ namespace OpenSkiJumping.ScriptableObjects
             suitBottomBackMaterial.SetColor(Color, SimpleColorPicker.Hex2Color(competitor.suitBottomBackColor));
             skisMaterial.SetColor(Color, SimpleColorPicker.Hex2Color(competitor.skisColor));
             LoadHelmetTexture();
+
         }
     }
 }
